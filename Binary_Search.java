0b081388e3068ddc8f0a5593a0b78b92fe7ed050
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-15 03:29:16 +0530

    Add files via upload
    Binary Search is a technique to search element in an array 
    start=0,end=length-1
    while(start<=end)
         find mid
         compare mid and key
            mid == key (return mid)
            mid<key  (follow the steps of binary search for the right or second half where start=mid+1 and end remains the same)
            mid>key  (follow the steps of binary search for the left or first half where end=mid-1 and start remains the same)

---
+++ Binary_Search.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Binary_Search {
+    public static int binarySearch(int arr[],int key){
+        int x = arr.length;
+        int start = 0;
+        int end = x-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid]>key){
+                end = mid-1;
+
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        System.out.println("Enter the elements of sorted array : ");
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.print("Enter the element to search : ");
+        int key=sc.nextInt();
+        int index = binarySearch(array, key);
+        if(index==-1){
+            System.out.println("element is not present in the array");
+        }else{
+            System.out.println("Element found at index : "+index);
+        }
+    }
+}
