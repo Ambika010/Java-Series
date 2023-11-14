7002963fe7ce284de4872b98a912b3a58d6f3634
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-14 22:04:59 +0530

    Add files via upload
    Linear Search is a technique to search element in an array by traversing each and every index from the 0th index.
    Time Complexity of linear search : O(n)

---
+++ Linear_Search.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Linear_Search {
+    public static int linearSearch(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.print("Enter the element to search : ");
+        int key=sc.nextInt();
+        int index = linearSearch(array, key);
+        if(index==-1){
+            System.out.println("element is not present in the array");
+        }else{
+            System.out.println("Element found at index : "+index);
+        }
+    }
+}
