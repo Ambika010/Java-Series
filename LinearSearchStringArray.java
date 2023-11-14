c50801fd803b3490a549eae6ce11966a38f21775
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-14 23:56:45 +0530

    Add files via upload
    This program performs linear search for a string array i.e. the array contains the elements in the forms of a string.

---
+++ LinearSearchStringArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class LinearSearchStringArray {
+    public static int linearSearchInString(String a[],String x){
+        for(int j=0;j<a.length;j++){
+            if(a[j].equals(x)){
+                return j;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        sc.nextLine();
+        String arr[]=new String[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.next();
+        }
+        System.out.print("Enter the element to search : ");
+        String x=sc.next();
+        int in = linearSearchInString(arr, x);
+        if(in==-1){
+            System.out.println("element is not present in the array");
+        }else{
+            System.out.println("Element found at index : "+in);
+        }
+    }
+}
