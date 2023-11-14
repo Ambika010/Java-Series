07d2192e92ea2d4d5a3046018044e60e3285bcd2
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-15 00:23:02 +0530

    Add files via upload
    This program prints the largest and the smallest element in an array.
    The point to remember : 
         For largest element, initialise largest = - infinity(Integer.MIN_VALUE) and then update the largest variable further
         For smallest element, initialise smalles = + infinity(Integer.MAX_VALUE) and then update the smallest variable further

---
+++ Largest_Smallest_In_Array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Largest_Smallest_In_Array {
+    public static int getLargest(int array[]){
+        int largest=Integer.MIN_VALUE; // -infinity or the smallest value in the range of integers in java
+        for(int i=0;i<array.length;i++){
+            if(array[i]>largest){
+                largest=array[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int getSmallest(int array[]){
+        int smallest=Integer.MAX_VALUE; // +infinity or the largest value in the range of integers in java
+        for(int i=0;i<array.length;i++){
+            if(array[i]<smallest){
+                smallest=array[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        sc.nextLine();
+        int array[]=new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Largest element in the array : "+getLargest(array));
+        System.out.println("Smallest element in the array : "+getSmallest(array));
+    }
+}
