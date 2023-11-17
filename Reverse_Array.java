14d757c2c7cf6142c9e957ee214b7574ec4badca
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-17 11:54:56 +0530

    Add files via upload
    This program is used to reverse an array 
    for example : 
    input : [ 2  4  6  8  10 ]
    output : [ 10  8  6  4  2 ]

---
+++ Reverse_Array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Reverse_Array {
+    public static void reverseArray(int arr[]){
+        int s=arr.length;
+        int first=0;
+        int last=s-1;
+        while(first < last){
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+            first++;
+            last--;
+        }
+        for(int i=0;i<s;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Reversed array : ");
+        reverseArray(array);
+    }
+}
