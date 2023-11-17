ad556cc4cb2b6cfddd7e5114212c957a846a280f
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-17 21:27:19 +0530

    Update All_Pairs_in_Array.java

--- All_Pairs_in_Array.java
+++ All_Pairs_in_Array.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class All_Pairs_in_Array {
     public static void arrayPairs(int arr[]){
         int tp=0;
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){ //this loop iterates from 0 to n-1
             int current = arr[i];
-            for(int j=i+1;j<arr.length;j++){
+            for(int j=i+1;j<arr.length;j++){ //this loop iterates from i+1 to print all possible pairs with i (it does not see values before i as the pairs must be unique)
                 int cur = arr[j];
                 System.out.print("("+current+","+cur+")");
                 tp++;
