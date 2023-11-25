050ab3165868cdde098386bf12ea95b7acbc61eb
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-25 22:19:31 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -19,6 +19,7 @@ public class Binary_Search {
         }
         return -1;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array : ");
