7ab05729a4bfb7c43dd91f44cde5fa549f7cec5c
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-12-27 21:20:40 +0530

    Add files via upload
    Prints the sum of all possible subarrays and then picks out the maximum sum out of it

---
+++ Max_Sum_Subarray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Max_Sum_Subarray{
+    public static void printSubarrays(int arr[]){
+        int ts=0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int start = i;
+            for(int j=i;j<arr.length;j++){
+                int end = j;
+                int sum=0;
+                for(int k=start;k<=end;k++){
+                    sum=sum+arr[k];
+                }
+                ts++;
+                System.out.print("Sum = "+sum);
+                System.out.println();
+                if(maxSum<sum){
+                    maxSum=sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Total Subarrays : "+ts);
+        System.out.println("Maximum Sum = "+maxSum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("All possible subarrays : ");
+        printSubarrays(array);
+    }
+}
