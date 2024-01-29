c048fcc02d7bc419e8aafe7fe8bfa0cf669f8fd8
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-01-29 21:40:59 +0530

    Add files via upload

---
+++ Pattern_7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Pattern_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("no. of rows : ");
+        int row = sc.nextInt();
+        for(int i=1;i<=row;i++){
+            int space = row-i;
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=row;j++){
+                if(i==1||i==row||j==1||j==row){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
